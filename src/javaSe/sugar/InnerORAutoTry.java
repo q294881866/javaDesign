dad8e47173a4f3aafea5f27e64d13aa4c53e3d39
ppf <294881866@qq.com>
2017-02-13 11:12:37 +0800

    命名规范

---
+++ src/javaSe/sugar/InnerORAutoTry.java
@@ -0,0 +1,32 @@
+package javaSe.sugar;
+
+import java.io.Closeable;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.InputStream;
+
+/**
+ * 
+ * @author ppf@jiumao.org
+ * @date 2017Äê1ÔÂ25ÈÕ
+ */
+public class InnerORAutoTry {
+
+	public class Inner{}
+	
+	public static class StaticInner{}
+	
+	public static void main(String[] args) {
+		//Stream implements Closeable
+		try(InputStream in = new FileInputStream(new File(""))) 
+		{new Thread(new Runnable() {
+			@Override
+			public void run() {
+				System.out.println("Anonymous Inner Class");
+			}
+		}).start(); } 
+		catch (Exception e) { }
+	}
+}
+
+class Another{}
