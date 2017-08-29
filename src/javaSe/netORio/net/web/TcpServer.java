6600bdf9190521c623642bc9872371300db5c184
ppf <shixi_pengfei5@staff.weibo.com>
2017-08-29 14:47:17 +0800

    Control concurrent times

--- src/javaSe/netORio/net/web/TcpServer.java
+++ src/javaSe/netORio/net/web/TcpServer.java
@@ -8,24 +8,46 @@ import java.io.PrintWriter;
 import java.io.Reader;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.Semaphore;
 
 import javaSe.basic.Domain.User;
 
+
 public class TcpServer {
-	public static void main(String[] args) throws Exception {
-		main0(args);
-	}
+    private static final Semaphore TRAFFIC_POLICE = new Semaphore(3);
+
+
+    public static void main(String[] args) throws Exception {
+        main0(args);
+    }
+
 
     private static void main0(String[] args) throws IOException {
         ServerSocket ss = new ServerSocket(10002);
-		Socket s = ss.accept();
-		
-		InboundHandler<Boolean> handler = new ObjectInboundHandler();
-		
-		handler.handler(s);
 
-		s.close();
-		ss.close();
+        shutdownPoint: while (true) {
+            try {
+                Socket s = ss.accept();
+                IPPool.add(s.getInetAddress());
+                TRAFFIC_POLICE.acquire();
+                InboundHandler<Boolean> handler = new ObjectInboundHandler();
+                Boolean status = handler.handler(s);
+                System.out.println(status);
+                TRAFFIC_POLICE.release();
+                s.close();
+            }
+            catch (Exception e) {
+                // bula bula bula ...
+                if (e instanceof ShutdownException) {
+                    break shutdownPoint;
+                }
+            }
+
+        }
+
+        ss.close();
     }
 
 }
